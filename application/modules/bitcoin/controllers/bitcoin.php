0cd55d9ff0357769b714b55a0073384391dfafe1
vman411gamer <vman678@gmail.com>
2015-02-12 15:08:33 -0600

    Update bitcoin.php

--- application/modules/bitcoin/controllers/bitcoin.php
+++ application/modules/bitcoin/controllers/bitcoin.php
@@ -84,7 +84,7 @@ class bitcoin extends MX_Controller {
 
             $email_msg = $this->load->view('InvoicePaid',$data,TRUE);
             $email_subject = '['.$this->config->item('company_name').' ] Purchase Confirmation';
-            $this->email->from("billing@vincenttaglia.com", 'Vincent Taglia - Web Design Payments');
+            $this->email->from($this->config->item('company_email'), $this->config->item('company_name');
             $this->email->to($client_email);
             $this->email->reply_to($this->config->item('company_email'), $this->config->item('company_name'));
             $this->email->subject($email_subject);
@@ -95,30 +95,6 @@ class bitcoin extends MX_Controller {
             $this->email->send();
    
     }
-	function send()
-	{
-		if ($_POST) {
-			$mail_to = 'vman678@gmail.com';
-			$subject = 'Bitcoin payment completed';
-
-			$body_message = '$'.$_POST['amount'].' in the amount of '.$_POST['btc_amount'].' BTC'."\n";
-			$body_message .= 'Invoice #'.$_POST['invoice_id']."\n";
-			$body_message .= 'Transaction ID: '.$_POST['txnum']."\n";
-
-			$headers = 'From: billing@vincenttaglia.com\r\n';
-
-			$mail_status = mail($mail_to, $subject, $body_message, $headers);
-			$this->session->set_flashdata('response_status', 'success');
-			$this->session->set_flashdata('message', 'Your invoice will be updated as "Paid" within the next 24 hours.');
-			redirect('clients');
-		}else{
-        $this->session->set_flashdata('response_status', 'error');
-        $this->session->set_flashdata('message', 'Something went wrong please contact us if your Payment doesn\'t appear shortly');
-        redirect('clients');
-        }
-		
-		
-	}
 	
 	
        function _log_activity($invoice_id,$activity,$icon,$user){
