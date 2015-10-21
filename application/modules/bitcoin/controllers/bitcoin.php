6283a80bc32c7893042c70ac8e34b2778b1e5e81
vman411gamer <vman678@gmail.com>
2015-10-21 15:39:37 -0500

    Cleaned up code

--- application/modules/bitcoin/controllers/bitcoin.php
+++ application/modules/bitcoin/controllers/bitcoin.php
@@ -15,6 +15,25 @@ class bitcoin extends MX_Controller {
 		$this->clients_table = 'companies';
 	}
 	
+	function curl_get_contents($url)
+	{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		$data = curl_exec($ch);
+		curl_close($ch);
+
+		return $data;
+	}
+	function round_up ( $value, $precision ) 
+	{ 
+		$pow = pow ( 10, $precision ); 
+		return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow;
+	}
+
 	function pay($invoice = NULL)
 	{
 		$userid = $this->tank_auth->get_user_id();
@@ -29,6 +48,14 @@ class bitcoin extends MX_Controller {
 										'currency' => $currency,
 										'amount' => $invoice_due) ;
 		$data['bitcoin'] = TRUE;
+		$urls = "https://blockchain.info/tobtc?currency=".$data['invoice_info']['currency']."&value=".$data['invoice_info']['amount'];
+		$btc_amount = $this->curl_get_contents($urls);
+		$data['btc_amount'] = $this->round_up($btc_amount, 3);
+		$blockchain_api = "https://blockchain.info/api/receive?api_code=7a4d9254-81ce-496d-98ce-8408082915c9&method=create&cors=true&format=plain&address=".$this->config->item('bitcoin_address')."&shared=false&callback=".base_url()."bitcoin%2Fsuccess%3Fusdamount%3D".$data['invoice_info']['amount']."%26invoicename%3D".$data['invoice_info']['item_name']."%26btcamount%3D".$btc_amount."%26invoice%3D".$data['invoice_info']['item_number']."%26client%3D".$userid;
+		$recieve_api = $this->curl_get_contents($blockchain_api);
+		
+		$decoded = json_decode($recieve_api);
+		$data['btc_address'] = $decoded->{'input_address'};
 		
 		$this->load->view('form',$data);
 	}
@@ -40,6 +67,7 @@ class bitcoin extends MX_Controller {
 	}
 	
 	function success(){
+		echo "*ok*";
 		function round_up ( $value, $precision ) { 
 			$pow = pow ( 10, $precision ); 
 			return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow;
@@ -72,19 +100,19 @@ class bitcoin extends MX_Controller {
 		$activity = lang('activity_payment_of').$this->user_profile->get_invoice_details($invoiceid,'currency').' '.$paid.lang('activity_payment_recieved_and_applied').$invoicename;
 
 		$this->_log_activity($invoiceid,$activity,$icon = 'fa-btc',$client); //log activity
-				echo "*ok*";
+				
 		$this->_notifyme($client_email,$client_username,$invoicename);
 		
 	}
 	function _notifyme($client_email,$client_username,$invoice_ref)
-    {
+	{
    
             $data['client_username'] = $client_username;
             $data['invoice_ref'] = $invoice_ref;
 
             $email_msg = $this->load->view('InvoicePaid',$data,TRUE);
             $email_subject = '['.$this->config->item('company_name').' ] Purchase Confirmation';
-            $this->email->from($this->config->item('company_email'), $this->config->item('company_name');
+            $this->email->from($this->config->item('company_email'), $this->config->item('company_name'));
             $this->email->to($client_email);
             $this->email->reply_to($this->config->item('company_email'), $this->config->item('company_name'));
             $this->email->subject($email_subject);
@@ -93,18 +121,20 @@ class bitcoin extends MX_Controller {
             $this->email->message_html($email_msg);
 
             $this->email->send();
-   
-    }
+		
+	}
 	
 	
-       function _log_activity($invoice_id,$activity,$icon,$user){
+       function _log_activity($invoice_id,$activity,$icon,$user)
+       {
             $this->db->set('module', 'invoices');
             $this->db->set('module_field_id', $invoice_id);
             $this->db->set('user', $user);
             $this->db->set('activity', $activity);
             $this->db->set('icon', $icon);
             $this->db->insert('activities'); 
-    }
+       	
+       }
 }
 
 
