712e60541297d932a47741edd3238ad6845940e5
vman411gamer <vman678@gmail.com>
2015-10-21 15:40:20 -0500

    Cleaned up view

--- application/modules/bitcoin/views/form.php
+++ application/modules/bitcoin/views/form.php
@@ -1,37 +1,10 @@
 <div class="modal-dialog">
 	<div class="modal-content">
-		<div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Paying <strong><?=$invoice_info['currency']?> <?=number_format($invoice_info['amount'],2)?></strong> for Invoice #<?=$invoice_info['item_name']?> Via Bitcoin</h4>
+		<div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Paying <strong><?=$invoice_info['currency']?> <?=number_format($invoice_info['amount'],2)?></strong> for Invoice #<?=$invoice_info['item_name']?> via Bitcoin</h4>
 		</div>		
 		<div class="modal-body">
 <?php
 		$attributes = array('id'=>'payment-form','class' => 'bs-example form-horizontal');
-		function curl_get_contents($url)
-		{
-			$ch = curl_init();
-
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_URL, $url);
-
-			$data = curl_exec($ch);
-			curl_close($ch);
-
-			return $data;
-		}
-		$this->load->library('tank_auth');
-		$client = $this->tank_auth->get_user_id();
-		function round_up ( $value, $precision ) { 
-			$pow = pow ( 10, $precision ); 
-			return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow;
-		}
-		$urls = "https://blockchain.info/tobtc?currency=".$invoice_info['currency']."&value=".$invoice_info['amount'];
-		$btc_amount = curl_get_contents($urls);
-		$btc_amount = round_up($btc_amount, 3);
-		$blockchain_api = "https://blockchain.info/api/receive?method=create&cors=true&format=plain&address=".$this->config->item('bitcoin_address')."&shared=false&callback=".base_url()."bitcoin%2Fsuccess%3Fusdamount%3D".$invoice_info['amount']."%26invoicename%3D".$invoice_info['item_name']."%26btcamount%3D".$btc_amount."%26invoice%3D".$invoice_info['item_number']."%26client%3D".$client;
-		$recieve_api = curl_get_contents($blockchain_api);
-		
-		$decoded = json_decode($recieve_api);
-		$btc_address = $decoded->input_address;
 		if (isset($errors) && !empty($errors) && is_array($errors)) {
 			echo '<div class="alert alert-error"><h4>Error!</h4>The following error(s) occurred:<ul>';
 			foreach ($errors as $e) {
